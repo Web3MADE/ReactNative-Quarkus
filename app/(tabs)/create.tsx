6c2d2f4d89dafb3bc608850d7c7eca59e3d6dddb
Hakeem <mwo248@yahoo.com>
2024-05-15 09:59:51 +0400

    remove video interaction logic

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -93,9 +93,7 @@ const Create = () => {
               <Video
                 source={{ uri: form.video.uri }}
                 className="w-full h-64"
-                useNativeControls
                 resizeMode={ResizeMode.COVER}
-                isLooping
               />
             ) : (
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
