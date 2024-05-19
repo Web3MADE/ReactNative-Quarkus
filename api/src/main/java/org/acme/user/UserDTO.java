622912ca6e1e70c72580683999f4756c74a10809
Hakeem <mwo248@yahoo.com>
2024-05-19 11:00:52 +0400

    include likedVideos in UserDTO

--- api/src/main/java/org/acme/user/UserDTO.java
+++ api/src/main/java/org/acme/user/UserDTO.java
@@ -4,19 +4,27 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import org.acme.video.VideoDTO;
 
+/** @dev UserDTO to be returned from API since it contains VideoDTOs within uploaded/liked videos */
 public class UserDTO {
     public Long id;
     public String name;
     public String email;
     public Set<VideoDTO> uploadedVideos;
+    public Set<VideoDTO> likedVideos;
 
     public UserDTO(User user) {
         this.id = user.id;
         this.name = user.name;
         this.email = user.email;
         if (user.uploadedVideos != null) {
+            // map each uploaded video to a VideoDTO
             this.uploadedVideos =
                     user.uploadedVideos.stream().map(VideoDTO::new).collect(Collectors.toSet());
+        }
+        if (user.likedVideos != null) {
+            // map each liked video to a VideoDTO
+            this.likedVideos =
+                    user.likedVideos.stream().map(VideoDTO::new).collect(Collectors.toSet());
         }
     }
 }
