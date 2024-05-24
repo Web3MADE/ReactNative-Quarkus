93318410833e6a9c2cc347d1dcc21aa59d55e809
Hakeem <mwo248@yahoo.com>
2024-05-24 14:28:42 +0400

    uploadVideo WIP (files not uploading)

--- app/hooks/useUploadVideo.ts
+++ app/hooks/useUploadVideo.ts
@@ -9,18 +9,64 @@ interface IUploadVideo {
   prompt: string;
 }
 
+const uriToBlob = async (uri: string) => {
+  try {
+    const response = await fetch(uri);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch file: ${response.statusText}`);
+    }
+    const blob = await response.blob();
+    return blob;
+  } catch (error) {
+    console.error("Error fetching file:", error);
+    throw error;
+  }
+};
+
+const uriToFile = async (uri: string, name: string, type: string) => {
+  try {
+    const blob = await uriToBlob(uri);
+    const file = new File([blob], name, { type });
+    return file;
+  } catch (error) {
+    console.error("Error converting Blob to File:", error);
+    throw error;
+  }
+};
+
 const uploadVideo = async (form: IUploadVideo) => {
   const formData = new FormData();
   formData.append("title", form.title);
   formData.append("uploaderId", "1"); // TODO: get user id from auth context
-  formData.append("video", form.video?.uri ?? "");
-  formData.append("thumbnail", form.thumbnail?.uri ?? "");
+  // TODO: uploaded files are null in the form
+  // formData.append("video", form.video?.uri ?? "");
+  // formData.append("thumbnail", form.thumbnail?.uri ?? "");
+  console.log("form.video?.uri ", form.video?.uri);
+  console.log("form.thumbnail?.uri ", form.thumbnail?.uri);
+
+  const videoFile = await uriToFile(
+    form.video?.uri ?? "",
+    "video.mp4",
+    "video/mp4"
+  );
+  const thumbnailFile = await uriToFile(
+    form.thumbnail?.uri ?? "",
+    "thumbnail.jpg",
+    "image/jpeg"
+  );
+  console.log("videoFile ", videoFile);
+  console.log("thumbnailFile ", thumbnailFile);
+  // TODO:
+  // formData not working with files, they contain data but the file itself if empty
+  // Moving forward, we need to ensure the API logic is tested with desired file types
+  // Then, the client side logic can be tested with the correct file types
+  formData.append("video", videoFile);
+  formData.append("thumbnail", thumbnailFile);
+
+  console.log("formData ", formData);
 
   await fetch("http://localhost:8080/api/videos", {
     method: "POST",
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
     body: formData,
   });
 };
