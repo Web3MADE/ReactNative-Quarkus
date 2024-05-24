318e6bd0fe71aeb9a388c7107e7c335dcb48e4b3
Hakeem <mwo248@yahoo.com>
2024-05-24 14:29:57 +0400

    findByTitle static method

--- api/src/main/java/org/acme/video/Video.java
+++ api/src/main/java/org/acme/video/Video.java
@@ -1,8 +1,10 @@
 package org.acme.video;
 
+import java.util.List;
 import java.util.Set;
 import org.acme.user.User;
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import io.smallrye.mutiny.Uni;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToMany;
@@ -12,13 +14,18 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "videos")
 public class Video extends PanacheEntity {
+    public static Uni<List<Video>> findByTitle(String query) {
+        return list("LOWER(title) LIKE LOWER(?1)", "%" + query + "%");
+    }
+
     public String title;
     public String url;
     public String thumbnailUrl;
-    public int likes;
 
+    public int likes;
     @ManyToOne(fetch = FetchType.EAGER)
     public User uploader;
+
     @ManyToMany(mappedBy = "likedVideos", fetch = FetchType.EAGER)
     public Set<User> likedByUsers;
 
