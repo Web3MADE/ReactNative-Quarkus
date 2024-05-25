0dc8b7c5d3b06b94345eabf57689ea8d3362c2c4
Hakeem <mwo248@yahoo.com>
2024-05-25 16:22:41 +0400

    User Repo + Service

---
+++ api/src/main/java/org/acme/repositories/UserRepository.java
@@ -0,0 +1,31 @@
+package org.acme.repositories;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.acme.user.User;
+import org.acme.user.UserDTO;
+import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserRepository implements PanacheRepository<User> {
+    public Uni<List<User>> getAllUsers() {
+        return User.listAll()
+                .map(users -> users.stream().map(user -> (User) user).collect(Collectors.toList()));
+    }
+
+    public Uni<User> getUserById(Long id) {
+        return User.findById(id).map(user -> ((User) user));
+    }
+
+    public Uni<UserDTO> createUser(UserDTO userDTO) {
+        User user = new User();
+        user.name = userDTO.name;
+        user.email = userDTO.getEmail();
+        user.password = userDTO.getPassword();
+        user.persist();
+        return Uni.createFrom().item(new UserDTO(user));
+    }
+
+}
---
+++ api/src/main/java/org/acme/services/UserService.java
@@ -0,0 +1,33 @@
+package org.acme.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.acme.repositories.UserRepository;
+import org.acme.user.User;
+import org.acme.user.UserDTO;
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+
+public class UserService {
+
+    @Inject
+    UserRepository userRepo;
+
+    public Uni<List<UserDTO>> getAllUsers() {
+        Uni<List<User>> users = userRepo.getAllUsers();
+        return users
+                .map(u -> u.stream().map(user -> new UserDTO(user)).collect(Collectors.toList()));
+    }
+
+    public Uni<UserDTO> getUserById(Long id) {
+        Uni<User> user = userRepo.getUserById(id);
+        return user.map(u -> new UserDTO(u));
+    }
+
+    public Uni<UserDTO> createUser(UserDTO userDTO) {
+        return userRepo.createUser(userDTO);
+    }
+
+
+
+}
