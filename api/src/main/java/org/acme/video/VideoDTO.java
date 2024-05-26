6dfa26087e446289d072c84db3c4cb2311dd0135
Hakeem <mwo248@yahoo.com>
2024-05-26 12:25:17 +0400

    update VideoDTO

--- api/src/main/java/org/acme/video/VideoDTO.java
+++ api/src/main/java/org/acme/video/VideoDTO.java
@@ -1,11 +1,11 @@
 package org.acme.video;
 
 public class VideoDTO {
-    public Long id;
-    public String title;
-    public String url;
-    public String thumbnailUrl;
-    public int likes;
+    private Long id;
+    private String title;
+    private String url;
+    private String thumbnailUrl;
+    private int likes;
 
     public VideoDTO(Video video) {
         this.id = video.id;
@@ -14,4 +14,48 @@ public class VideoDTO {
         this.thumbnailUrl = video.thumbnailUrl;
         this.likes = video.likes;
     }
+
+    public VideoDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+
 }
