803afb074c9941bcd9e239d65d08935528b3fd48
Hakeem <mwo248@yahoo.com>
2024-05-26 09:51:55 +0400

    SOLID UserService

--- api/src/main/java/org/acme/services/UserService.java
+++ api/src/main/java/org/acme/services/UserService.java
@@ -5,23 +5,25 @@ import java.util.stream.Collectors;
 import org.acme.repositories.UserRepository;
 import org.acme.user.User;
 import org.acme.user.UserDTO;
+import org.acme.video.VideoDTO;
 import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+@ApplicationScoped
 public class UserService {
 
     @Inject
     UserRepository userRepo;
 
     public Uni<List<UserDTO>> getAllUsers() {
-        Uni<List<User>> users = userRepo.getAllUsers();
-        return users
-                .map(u -> u.stream().map(user -> new UserDTO(user)).collect(Collectors.toList()));
+        return userRepo.getAllUsers().map(
+                // this::mapUserToUserDTO is a method reference to mapUserToUserDTO
+                users -> users.stream().map(this::mapUserToUserDTO).collect(Collectors.toList()));
     }
 
     public Uni<UserDTO> getUserById(Long id) {
-        Uni<User> user = userRepo.getUserById(id);
-        return user.map(u -> new UserDTO(u));
+        return userRepo.getUserById(id).map(this::mapUserToUserDTO);
     }
 
     public Uni<UserDTO> createUser(UserDTO userDTO) {
@@ -29,7 +31,38 @@ public class UserService {
     }
 
     public Uni<UserDTO> findByEmail(String email) {
-        Uni<User> user = userRepo.findByEmail(email);
-        return user.map(u -> new UserDTO(u));
+        return userRepo.findByEmail(email).map(this::mapUserToUserDTO);
+    }
+
+    private UserDTO mapUserToUserDTO(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.id);
+        userDTO.setName(user.name);
+        userDTO.setEmail(user.email);
+        userDTO.setPassword(user.password);
+        userDTO.setUploadedVideos(
+                user.uploadedVideos.stream().map(VideoDTO::new).collect(Collectors.toSet()));
+        userDTO.setLikedVideos(
+                user.likedVideos.stream().map(VideoDTO::new).collect(Collectors.toSet()));
+
+        return userDTO;
+    }
+
+    private User mapUserDTOtoUser(UserDTO userDTO) {
+        if (userDTO == null) {
+            return null;
+        }
+
+        User user = new User();
+        user.id = userDTO.getId();
+        user.name = userDTO.getName();
+        user.email = userDTO.getEmail();
+        user.password = userDTO.getPassword(); // TODO: hash password func
+
+        return user;
     }
 }
