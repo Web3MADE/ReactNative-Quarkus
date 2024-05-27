945804201452af71133e3ed2b6a14916ec9d4de1
Hakeem <mwo248@yahoo.com>
2024-05-27 09:45:22 +0400

    constants for JWT

--- api/src/main/java/org/acme/utils/Constants.java
+++ api/src/main/java/org/acme/utils/Constants.java
@@ -1,7 +1,15 @@
 package org.acme.utils;
 
 public class Constants {
+    public enum Role {
+        USER, ADMIN
+    }
+
     public static final String CONTAINER_NAME = "container-quarkus-azure-storage-blob-async";
+    public static final String JWT_ISSUER_URL = "https://example.com/issuer";
+
+    public static final String JWT_BIRTHDATE = "2001-07-13";
+
 
     private Constants() {}
 }
