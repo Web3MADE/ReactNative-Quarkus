18198fa17735b9332271546e2c930b89bc5f1326
Hakeem <mwo248@yahoo.com>
2024-05-27 08:51:37 +0400

    userController getAllUsers test

---
+++ api/src/test/java/org/acme/controllers/UserControllerTest.java
@@ -0,0 +1,56 @@
+package org.acme.controllers;
+
+import static io.restassured.RestAssured.given;
+import static org.mockito.Mockito.when;
+import java.util.List;
+import org.acme.services.UserService;
+import org.acme.user.User;
+import org.acme.user.UserDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.vertx.RunOnVertxContext;
+import io.quarkus.test.vertx.UniAsserter;
+import io.smallrye.mutiny.Uni;
+
+@QuarkusTest
+@TestHTTPEndpoint(UserController.class)
+public class UserControllerTest {
+
+    @InjectMock
+    UserService userService;
+
+    private User user;
+    private UserDTO userDTO;
+
+    @BeforeEach
+    public void setUp() {
+        // Set up mock user
+        user = new User();
+        user.id = 1L;
+        user.name = "test";
+        user.email = "test@example.com";
+        user.password = "test";
+        // Set up mock userDTO
+        userDTO = new UserDTO(user);
+    }
+
+
+
+    @RunOnVertxContext
+    @Test
+    void testGetAllUsers(UniAsserter asserter) {
+        asserter.execute(() -> {
+            when(userService.getAllUsers()).thenReturn(Uni.createFrom().item(List.of(userDTO)));
+            return Uni.createFrom().voidItem();
+        });
+        given().when().get().then().statusCode(200);
+        Mockito.verify(userService, Mockito.times(1)).getAllUsers();
+    }
+
+
+
+}
